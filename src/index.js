bd2650ebeceb314e3ccf1d17760bda6a8bd9cb89
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-03-08 19:31:18 +0100

    list todo done

--- src/index.js
+++ src/index.js
@@ -10,10 +10,12 @@ let todos = [
   {
     text: "Je suis un todo",
     done: true,
+    editMode: false,
   },
   {
     text: "Demain, je vais faire le JavaScript",
     done: false,
+    editMode: true,
   },
 ];
 
@@ -28,7 +30,11 @@ form.addEventListener("submit", (event) => {
 const displayTodo = () => {
   // On recuprer ce tableau, et on affiche sur le HTML
   const todosNode = todos.map((todo, index) => {
-    return createToDoElement(todo, index);
+    if (todo.editMode) {
+      return createToDoEditElement(todo, index);
+    } else {
+      return createToDoElement(todo, index);
+    }
   });
 
   ul.innerHTML = "";
@@ -40,8 +46,15 @@ const createToDoElement = (todo, index) => {
   const li = document.createElement("li");
   const deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Supprimer";
+  const editButton = document.createElement("button");
+  editButton.innerHTML = "Modification";
   deleteButton.addEventListener("click", (event) => {
+    event.stopPropagation();
     deleteToDo(index);
+  });
+  editButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    toggleEditMode(index);
   });
   li.innerHTML = `
   <span class="todo ${todo.done ? "done" : ""}"></span>
@@ -49,8 +62,32 @@ const createToDoElement = (todo, index) => {
     todo.text.charAt(0).toUpperCase() + todo.text.slice(1).toLowerCase()
   }</p> 
   `;
-  li.appendChild(deleteButton);
+  li.append(deleteButton, editButton);
+  li.addEventListener("click", () => {
+    toggleTodo(index);
+  });
+
   // On va retourner li qui corresponds au codeHTML complet qu'on va utiliser pour afficher plus haut
+  return li;
+};
+
+const createToDoEditElement = (todo, index) => {
+  const li = document.createElement("li");
+  const input = document.createElement("input");
+  input.type = "text";
+  input.value = todo.text;
+  const saveButton = document.createElement("button");
+  saveButton.innerHTML = "Save";
+  const cancelButton = document.createElement("Button");
+  cancelButton.innerHTML = "Cancel";
+  cancelButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    toggleEditMode(index);
+  });
+  saveButton.addEventListener("click", (event) => {
+    editToDo(index, input);
+  });
+  li.append(input, cancelButton, saveButton);
   return li;
 };
 
@@ -66,6 +103,23 @@ const addToDo = (text) => {
 const deleteToDo = (index) => {
   todos.splice(index, 1);
 
+  displayTodo();
+};
+
+const toggleTodo = (index) => {
+  todos[index].done = !todos[index].done;
+  displayTodo();
+};
+
+const toggleEditMode = (index) => {
+  todos[index].editMode = !todos[index].editMode;
+  displayTodo();
+};
+
+const editToDo = (index, input) => {
+  const value = input.value;
+  todos[index].text = value;
+  todos[index].editMode = false;
   displayTodo();
 };
 
