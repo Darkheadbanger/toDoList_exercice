df9a9c3cbe8c3aa3960e125ad407e0f8d0bb923d
David Bouhaben <davidbouhaben@yahoo.co.id>
2022-03-10 17:00:44 +0100

    localstorage persiste and delete works

---
+++ src/asynchrone.js
@@ -0,0 +1,10 @@
+// setTimeOut
+
+console.log(1);
+setTimeout(() => {
+  console.log(2);
+}, 10);
+console.log(3);
+
+// promesse
+// Exo asynchrone, settimeout et voir les promesses
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-
+import "./asynchrone";
 // On récupère le ul pour pouvoir afficheer dans la liste
 const ul = document.querySelector("ul");
 const form = document.querySelector("form");
@@ -12,17 +12,14 @@ let todos = [
     done: true,
     editMode: false,
   },
-  {
-    text: "Demain, je vais faire le JavaScript",
-    done: false,
-    editMode: true,
-  },
 ];
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const value = input.value;
-  input.value = "";
+  if (value) {
+    input.value = "";
+  }
   addToDo(value);
 });
 
@@ -30,13 +27,13 @@ form.addEventListener("submit", (event) => {
 const displayTodo = () => {
   // On recuprer ce tableau, et on affiche sur le HTML
   const todosNode = todos.map((todo, index) => {
+    // Si edite mode est true alors, on peut le modifier
     if (todo.editMode) {
       return createToDoEditElement(todo, index);
     } else {
       return createToDoElement(todo, index);
     }
   });
-
   ul.innerHTML = "";
   ul.append(...todosNode);
 };
@@ -45,12 +42,16 @@ const displayTodo = () => {
 const createToDoElement = (todo, index) => {
   const li = document.createElement("li");
   const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = "Supprimer";
+  deleteButton.innerText = "Supprimer";
   const editButton = document.createElement("button");
-  editButton.innerHTML = "Modification";
+  editButton.innerText = "Modification";
   deleteButton.addEventListener("click", (event) => {
     event.stopPropagation();
-    deleteToDo(index);
+    if (confirm("Est ce que vous êtes sûr de vouloir effacer cette tâches ?")) {
+      deleteToDo(todo, index);
+    } else {
+      return;
+    }
   });
   editButton.addEventListener("click", (event) => {
     event.stopPropagation();
@@ -62,6 +63,7 @@ const createToDoElement = (todo, index) => {
     todo.text.charAt(0).toUpperCase() + todo.text.slice(1).toLowerCase()
   }</p> 
   `;
+
   li.append(deleteButton, editButton);
   li.addEventListener("click", () => {
     toggleTodo(index);
@@ -85,24 +87,34 @@ const createToDoEditElement = (todo, index) => {
     toggleEditMode(index);
   });
   saveButton.addEventListener("click", (event) => {
-    editToDo(index, input);
+    event.stopPropagation();
+    if (confirm("Êtes vous sur de vouloir modifier cette tâches ?")) {
+      editToDo(index, input);
+    } else {
+      return;
+    }
   });
   li.append(input, cancelButton, saveButton);
   return li;
 };
 
 const addToDo = (text) => {
-  todos.push({
-    text,
-    done: false,
-  });
-
-  displayTodo();
+  console.log(text);
+  if (!text) {
+    return alert("Veuillez remplir le champ!");
+  } else {
+    todos.push({
+      text,
+      done: false,
+    });
+    localSaved();
+    displayTodo();
+  }
 };
 
 const deleteToDo = (index) => {
   todos.splice(index, 1);
-
+  deleteDataFromLocalStorage(index);
   displayTodo();
 };
 
@@ -123,4 +135,41 @@ const editToDo = (index, input) => {
   displayTodo();
 };
 
+const localSaved = () => {
+  console.log("ici todo :", todos);
+  if (JSON.parse(localStorage.getItem("todo-element")) === null) {
+    localStorage.setItem("todo-element", JSON.stringify(todos));
+  } else {
+    localStorage.setItem("todo-element", JSON.stringify(todos));
+  }
+};
+
+const retrieveLocalStorage = () => {
+  if (JSON.parse(localStorage.getItem("todo-element")) !== null) {
+    todos = JSON.parse(localStorage.getItem("todo-element"));
+  }
+  // Toujours afficher le displayTodo c'est a dire, afficher tent que le données du localStorage active
+  displayTodo();
+};
+const deleteDataFromLocalStorage = (index) => {
+  console.log("index ici :", index);
+  const store = JSON.parse(localStorage.getItem("todo-element")) || [];
+  store.splice(index, 1);
+  localStorage.setItem("todo-element", JSON.stringify(todos));
+  displayTodo();
+};
 displayTodo();
+// Il faut toujours mettre le localStorage en global (pas dans une fonction) pour le faire fonctionner
+retrieveLocalStorage();
+
+// Exo Todo et BONNNNNNNEEEE CHHAAAAANNNCEEEEEEEEEEEEEE!!!!!!!!!!!!!!
+// 1. d'empecher l'ajout de todo vide (fait)
+// 2. LocalStorage et que ca reste (fait)
+// 3. d'empecher d'ajouter le même tache
+// 3. Mettre une majuscule a la premiere lettre du texte de la todo
+// 4. Barré le text de la todo lorsqu'elle est terminer
+// 5.Ajouter un curseur pointer sur les todos
+// 6. Ajouter un effet hover sur les todos pour mieux voir sur laquelle l'utilisateur va cliquer
+// 7. Créer des classes pour colorer les boutons
+// 8. a voir : Lors de l'édition, ajoutez un écouteur pour sauvegarder avec la touche entrée si le champ est focus ou annuler avec la touche escape.
+// 9. a voir : Rendre l'édition possible sur un double clic.
